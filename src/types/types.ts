2b42b12215f34e07163ad968d06d3c34349f9b9e
Daniel Bartels <sanbartels@gmail.com>
2024-05-10 20:19:19 -0500

    feat: add new component for displaying messages  The code changes include: - Added a new component called "Mensaje.astro" for displaying messages. - Defined the Props interface for the "Mensaje" component. - Extracted the "nombre" prop from the Astro props object in the "Mensaje" component. - Rendered the "nombre" prop inside an h1 tag in the "Mensaje" component.  This commit adds a new component that will be used to display messages in the application.

---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+export interface MenuItem {
+    title: string;
+    url: string;
+    icon: string;
+}
+
+export interface CardItem {
+    title: string;
+    url: string;
+    description: string;
+}
+
+export interface faq {
+    question: string;
+    answer: string;
+}
